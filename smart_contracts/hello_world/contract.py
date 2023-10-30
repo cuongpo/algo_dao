0b9cd8394ec930d9a4497985312f7afcc9ac20da
cuongquep <vucuong.marketing@gmail.com>
2023-10-30 17:42:56 +0700

    init

---
+++ smart_contracts/hello_world/contract.py
@@ -0,0 +1,127 @@
+from beaker import *
+from pyteal import *
+
+from beaker.lib.storage import BoxMapping
+
+
+class proposalStruct(abi.NamedTuple):
+    description: abi.Field[abi.String]
+    vote_yes: abi.Field[abi.Uint64]
+    vote_no: abi.Field[abi.Uint64]
+    status: abi.Field[abi.String]
+
+
+class AppStateValue:
+    memberCount = GlobalStateValue(
+        stack_type=TealType.uint64,
+        default=Int(0),
+        descr="Number Member of DAO",
+    )
+
+    proposalCount = GlobalStateValue(
+        stack_type=TealType.uint64, default=Int(0), descr="Number of Proposals"
+    )
+
+    memberDAO = LocalStateValue(
+        stack_type=TealType.uint64,
+        default=Int(1),
+        descr="Member DAO or not",
+    )
+
+    proposals = BoxMapping(abi.Uint64, proposalStruct)
+
+
+app = (
+    Application("Simple DAO", state=AppStateValue())
+    .apply(unconditional_create_approval, initialize_global_state=True)
+    .apply(unconditional_opt_in_approval, initialize_local_state=True)
+)
+
+
+## Check member dao or not
+@app.external
+def check_member_dao(*, output: abi.Uint64) -> Expr:
+    return output.set(app.state.memberDAO[Txn.sender()])
+
+
+## Join DAO
+@app.external
+def join_dao() -> Expr:
+    return Seq(
+        app.state.memberDAO[Txn.sender()].set(Int(0)),
+        app.state.memberCount.increment(),
+    )
+
+
+## Leave DAO
+@app.external
+def leave_dao() -> Expr:
+    return Seq(
+        app.state.memberDAO[Txn.sender()].set(Int(1)),
+        app.state.memberCount.decrement(),
+    )
+
+
+## Create Proposal
+@app.external
+def create_proposal(descr: abi.String, *, output: proposalStruct) -> Expr:
+    proposal_tuple = proposalStruct()
+    proposalId = abi.Uint64()
+    vote_yes = abi.Uint64()
+    vote_no = abi.Uint64()
+    status = abi.String()
+    return Seq(
+        vote_yes.set(Int(0)),
+        vote_no.set(Int(0)),
+        status.set("In Progress"),
+        proposal_tuple.set(descr, vote_yes, vote_no, status),
+        proposalId.set(app.state.proposalCount.get()),
+        app.state.proposals[proposalId].set(proposal_tuple),
+        app.state.proposalCount.increment(),
+        app.state.proposals[proposalId].store_into(output),
+    )
+
+
+## Check Proposal
+@app.external
+def check_proposal(proposalId: abi.Uint64, *, output: proposalStruct) -> Expr:
+    return app.state.proposals[proposalId].store_into(output)
+
+
+## End Proposal when total vote > 1/2 member Count
+@app.external
+def end_proposal(proposalId: abi.Uint64, *, output: abi.String) -> Expr:
+    proposal = proposalStruct()
+    description = abi.String()
+    vote_yes = abi.Uint64()
+    vote_no = abi.Uint64()
+    status = abi.String()
+    new_status = abi.String()
+
+    total_dao_member = app.state.memberCount.get()
+    app.state.proposals[proposalId].store_into(proposal)
+    description.set(proposal.description)
+    vote_yes.set(proposal.vote_yes)
+    vote_no.set(proposal.vote_no)
+    status.set(proposal.status)
+
+    if status != "In Progress":
+        return output.set("Proposal Ended")
+
+    if 2 * (vote_yes + vote_no) < total_dao_member:
+        return output.set("not enough vote")
+
+    if vote_yes < vote_no:
+        return Seq(
+            output.set("proposal failed"),
+            new_status.set("proposal failed"),
+            proposal.set(description, vote_yes, vote_no, new_status),
+            app.state.proposals[proposalId].set(proposal),
+        )
+
+    return Seq(
+        output.set("proposal success"),
+        new_status.set("proposal success"),
+        proposal.set(description, vote_yes, vote_no, new_status),
+        app.state.proposals[proposalId].set(proposal),
+    )
